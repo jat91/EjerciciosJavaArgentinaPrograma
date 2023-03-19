dcaf1fc84dddf94896788d2b3987607a6ac3757c
JAT91 <joaquintravia@hotmail.com>
2023-03-19 14:52:23 -0300

    Ejercicio C

--- src/Ejercicios/Clase3_1_C.java
+++ src/Ejercicios/Clase3_1_C.java
@@ -1,15 +1,19 @@
 package Ejercicios;
+import java.util.Random;
 
 public class Clase3_1_C {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		int numeros[] = new int[] {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};
+		Random random = new Random();
+		int numeros[] = new int[10];
 		
-		for(int i=0;i < numeros.length;i++) {
-			System.out.println(numeros[i]+5);
+		for(int i=0;i < 10;i++) {
+			numeros[i] = random.nextInt(5000);
 		}
+		
+		float resultado = (float)numeros[5] / 3;
+		System.out.println("El sexto numero del array es "+numeros[5]+" y divido por 3 da "+ resultado);
 	}
 
 }
